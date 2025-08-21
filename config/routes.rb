a45d33f9639a4228174bd316c03c5201f0f5de54
Lena Geisler <lenageisler@gmx.de>
2025-08-21 15:01:47 +0200

    configuration of bookmark routes, actions and new view

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "lists", to: "lists#index", as: :lists
-  get "lists/new", to: "lists#new"
-  post "lists", to: "lists#create"
-  get "lists/:id", to: "lists#show", as: :list
+
+  # get "lists", to: "lists#index", as: :lists
+  # get "lists/new", to: "lists#new"
+  # post "lists", to: "lists#create"
+  # get "lists/:id", to: "lists#show", as: :list
+  # get "lists/:list_id/bookmarks/new", to: "bookmarks#new", as: :new_bookmark
+  # post "lists/:list_id/bookmarks", to: "bookmarks#create", as: :create_bookmark
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
+
 end
